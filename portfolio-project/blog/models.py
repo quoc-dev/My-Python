2de944e8a44f32867c96ec0cd1879103af0f4320
at-quocnguyen <quoc.nguyen@asiantech.vn>
2018-05-20 21:34:23 +0700

    home page

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+# Create Blog model
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_day = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(up_load='images/')
+
+# Add the blog app to the settings
+
+
+# Create a migration
+
+
+# Migrate
+
+
+# Add to admin
