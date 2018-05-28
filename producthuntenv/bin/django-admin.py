82722c67a20b99954acb8740911e817e848cdc0d
at-quocnguyen <quoc.nguyen@asiantech.vn>
2018-05-28 22:23:08 +0700

    new project producthunt

---
+++ producthuntenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/quocnguyenp./QuocDEV/MyPy/My-Python/producthuntenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
