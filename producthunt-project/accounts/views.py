9d8bf79e91dd0fa879f4f7250d7432cfc4842fc5
at-quocnguyen <quoc.nguyen@asiantech.vn>
2018-06-03 22:52:35 +0700

    finsih login and logout

--- producthunt-project/accounts/views.py
+++ producthunt-project/accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == 'POST':
+        # The user has info and wants an accounts now
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'signup.html', {'error': 'Username already has been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'signup.html', {'error': 'Password must match'})
+
+    else:
+        # User want to enter info
+        return render(request, 'signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    # TODO need to route to homepage
+    return render(request, 'signup.html')
