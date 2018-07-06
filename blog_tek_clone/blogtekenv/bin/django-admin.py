8056ac2ea5f282272139002ff5be2e40da8b96b0
quoc-dev <phuquoc.dev@gmail.com>
2018-07-06 20:06:31 +0700

    delete

--- blog_tek_clone/blogtekenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/quocnguyenp./QuocDEV/MyPy/My-Python/blog_tek_clone/blogtekenv/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
